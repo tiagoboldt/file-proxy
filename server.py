2950922b78d348c878d4c501bc38ac534faf8375
Tiago Boldt Sousa <tiagoboldt@gmail.com>
2017-11-17 23:07:46 +0000

    receive stream hash as argument and retry on stream failure

--- server.py
+++ server.py
@@ -1,15 +1,26 @@
 from flask import Flask, Response
+import sys
 import requests
 app = Flask(__name__)
 
 url = None  # Stream address
 endpoint = '/'  # server endpoint
 
+url = 'http://127.0.0.1:8000/pid/%s/stream.mp4' % (sys.argv[1])
+endpoint = '/video_feed.mp4'
+
 
 def gen():
-    r = requests.get(url, stream=True)
-    for chunk in r.iter_content(chunk_size=1024*10):
-        yield(chunk)
+    first = True
+    while True:
+        with requests.get(url, stream=True, timeout=60) as r:
+            for chunk in r.iter_content(chunk_size=1024*50):
+                if first:
+                    first = False
+                    print(chunk)
+                yield(chunk)
+        print('retrying')
+        first = True
 
 
 @app.route(endpoint)
@@ -18,4 +29,6 @@ def video_feed():
 
 
 if __name__ == '__main__':
+    print('using: %s' % url)
+    print('use: http://localhost:5000%s' % endpoint)
     app.run(host='0.0.0.0', debug=True)
